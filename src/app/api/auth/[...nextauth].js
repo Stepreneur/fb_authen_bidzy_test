c005ea953035e19a4a6cee1b228d99c069e126d4
Stepreneur <kfranzaza13@gmail.com>
2025-07-03 21:11:05 +0700

    re

--- src/app/api/auth/[...nextauth].js
+++ src/app/api/auth/[...nextauth].js
@@ -1,8 +1,12 @@
+import NextAuth from "next-auth";
 import FacebookProvider from "next-auth/providers/facebook";
 
-providers: [
-  FacebookProvider({
-    clientId: process.env.FACEBOOK_CLIENT_ID,
-    clientSecret: process.env.FACEBOOK_CLIENT_SECRET
-  })
-]
+export default NextAuth({
+  providers: [
+    FacebookProvider({
+      clientId: process.env.FACEBOOK_CLIENT_ID,
+      clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
+    }),
+  ],
+  // ...other config (callbacks, pages, etc.)
+});
